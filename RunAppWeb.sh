d420a0cf160f40d09d2bcde120cd501ec17ecf9d
Luis Ramos Galindo <rgalindo.luis@gmail.com>
2019-12-15 17:07:53 -0500

    first commit

---
+++ RunAppWeb.sh
@@ -0,0 +1,8 @@
+# Compilación y ejecución de aplicación web
+# que muestra mensaje de saludo, opcionalmente recibe parámetro "name"
+
+# Compilamos
+./mvnw clean package
+
+# Ejecutamos 
+./mvnw spring-boot:run.
---
+++ TestAppWeb.sh
@@ -0,0 +1,9 @@
+# Compilación y ejecución en modo de pruebas de aplicación web
+# Aplicación web que muestra mensaje de saludo, opcionalmente recibe parámetro "mame"
+
+# Compilamos la aplicación
+./mvnw clean package 
+
+
+# Ejecutamos las pruebas
+./mnvw test
